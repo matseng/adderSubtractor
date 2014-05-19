4f8526f1f1339604580d9481c292d3c1481fc8d0
Michael Tseng <matseng@gmail.com>
2014-05-19 10:07:41 -0700

    MVP of subtract button

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
-  function getX() {
-    var xStr = document.getElementById('inputX').value;
-    return parseInt(xStr);
-  };
+function getX() {
+  var xStr = document.getElementById('inputX').value;
+  return parseInt(xStr);
+};
 
-  function getY() {
-    var yStr = document.getElementById('inputY').value;
-    return parseInt(yStr);
-  };
+function getY() {
+  var yStr = document.getElementById('inputY').value;
+  return parseInt(yStr);
+};
 
-  function setZ(val) {
-    document.getElementById('valueZ').textContent = val;
-  };
+function setZ(val) {
+  document.getElementById('valueZ').textContent = val;
+};
 
-  function addButtonClicked() {
-    var z = getX() + getY();
-    setZ(z);
-  };
+function addButtonClicked() {
+  var z = getX() + getY();
+  setZ(z);
+};
+
+function subtractButtonClicked() {
+  var z = getX() - getY();
+  setZ(z);
+};
