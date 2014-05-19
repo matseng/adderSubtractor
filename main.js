d1968ecca376522429062d014f35f0b0362ec452
Michael Tseng <matseng@gmail.com>
2014-05-19 10:01:14 -0700

    Completed MVP of adder

---
+++ main.js
@@ -0,0 +1,18 @@
+  function getX() {
+    var xStr = document.getElementById('inputX').value;
+    return parseInt(xStr);
+  };
+
+  function getY() {
+    var yStr = document.getElementById('inputY').value;
+    return parseInt(yStr);
+  };
+
+  function setZ(val) {
+    document.getElementById('valueZ').textContent = val;
+  };
+
+  function addButtonClicked() {
+    var z = getX() + getY();
+    setZ(z);
+  };
