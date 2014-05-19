c4ac57a58bb08fd1caa325a999a9944718bc4f05
Michael Tseng <matseng@gmail.com>
2014-05-19 12:55:31 -0700

    Used addEventListener with 'keyup' to detect a change AFTER the new value has been registered

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
-function getX() {
-  var xStr = document.getElementById('inputX').value;
-  return parseInt(xStr);
-};
+window.onload = function() {
 
-function getY() {
-  var yStr = document.getElementById('inputY').value;
-  return parseInt(yStr);
-};
+  function getX() {
+    var xStr = document.getElementById('inputX').value;
+    return parseInt(xStr);
+  };
 
-function setZ(val) {
-  document.getElementById('valueZ').textContent = val;
-};
+  function getY() {
+    var yStr = document.getElementById('inputY').value;
+    return parseInt(yStr);
+  };
 
-function addButtonClicked() {
-  var z = getX() + getY();
-  setZ(z);
-};
+  function setZ(val) {
+    document.getElementById('valueZ').textContent = val;
+  };
 
-function subtractButtonClicked() {
-  var z = getX() - getY();
-  setZ(z);
+  document.getElementById('addButton').addEventListener('click', addButtonClicked);
+  function addButtonClicked() {
+    var z = getX() + getY();
+    setZ(z);
+  };
+
+  document.getElementById('subtractButton').addEventListener('click', subtractButtonClicked);
+  function subtractButtonClicked() {
+    var z = getX() - getY();
+    setZ(z);
+  };
+
+  document.getElementById('inputX').addEventListener('keyup', function() {
+    var myNum = getX();
+    console.log(myNum);
+    if(!Number.isInteger(getX())){
+      console.log(getX());
+      console.log('please enter an integer');
+    }
+  });
+
+  function checkInt(val) {
+    return Number.isInteger(val);
+  }
+
 };
